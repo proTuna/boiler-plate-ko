0c89b7e24220ebd94a5198cc9b6572e43f5786ad
proTuna <tunaman629@gmail.com>
2021-05-25 16:59:23 +0900

    change security

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser');
 
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://choi:7147@boilerplate.9flua.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+const confing = require('./config/key');
+
+const { User } = require('./models/User');
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended:true}));
+
+// application/json
+app.use(bodyParser.json());
+
+const mongoose = require('mongoose');
+mongoose.connect(confing.mongoURI,{
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log("MongoDB Conneted..."))
-.catch( err => console.log(err))
+.catch( err => console.log(err));
 
-app.get('/',(req, res) => res.send('Hello World!'))
+app.get('/',(req, res) => res.send('Hello World! t'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.post('/register', (req, res) => {
+
+    // 회원 가입 할때 필요한 정보를 clien에서 가져오면
+    // 그것들을 DB에 넣어준다.
+    const user = new User(req.body);
+
+    user.save((err, doc) => {
+        if(err) return res.json({ success: false, err})
+        return res.status(200).json({success:true})
+    });
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
